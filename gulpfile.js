b5e5dac6557c8e57dea97dfa6cb8f819b702e8e0
Peter Mouland <peter.mouland@gmail.com>
2014-09-08 13:14:28 +0100

    added styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('sass', function () {
+    gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/css'));
 });
