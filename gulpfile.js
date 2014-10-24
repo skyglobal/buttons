46bd87570ed365e05822f02aa3803d65dba8978b
Peter Mouland <peter.mouland@gmail.com>
2014-10-24 11:47:41 +0100

    tidy up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,8 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var compass = require('gulp-compass');
 var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer');
 var runSequence = require('run-sequence');
 var deploy = require("gulp-gh-pages");
 var run = require("gulp-run");
@@ -13,15 +12,8 @@ var pkg = require('./package.json');
 var aws_s3 = require('gulp-aws-s3').setup({bucket: process.env.AWS_SKYGLOBAL_BUCKET});
 
 var paths= {
-    "deploy-remote": "origin",
-    "deploy-branch": "gh-pages",
-    "imagesSrc": "_images",
-    "assets": "_site/assets",
-    "jekyll": [
-        "**/*.html",
-        "**/*.md",
-        "!_site/**/*.html",
-        "!node_modules/**/*"
+    "site": [
+        "demo/*.html"
     ],
     sass: 'scss',
     "sass-demo": 'demo/scss',
@@ -36,6 +28,7 @@ gulp.task('sass', function() {
             includePaths: ['scss','bower_components'],
             outputStyle: 'compressed'
         }))
+        .pipe(autoprefixer())
         .pipe(gulp.dest(paths.css))
         .pipe(browserSync.reload({stream:true}));
 });
@@ -52,26 +45,12 @@ gulp.task('browserSync', function() {
 
 
 gulp.task('watch', function() {
-//    gulp.watch(paths.jekyll, ['jekyll-rebuild']);
+    gulp.watch(paths.site, ['create-site']);
     gulp.watch([paths['sass'] + '/**/*.scss',paths['sass-demo'] + '/**/*.scss'], ['sass']);
 });
 
-
-
-//gulp.task('jekyll-build', function (done) {
-//    return childProcess.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
-//        .on('close', done);
-//});
-//
-//gulp.task('jekyll-rebuild', function() {
-//    return runSequence(['jekyll-build','sass'], function () {
-//        browserSync.reload();
-//    });
-//});
-
-
 gulp.task('build', function(cb) {
-    return runSequence(['sass', 'create-site'],
+    return runSequence('bower', ['sass', 'create-site'],
         cb
     );
 });
@@ -95,26 +74,18 @@ gulp.task('create-site', function() {
 });
 
 gulp.task('aws', function() {
-    gulp.src(['scss/buttons.scss','_site/css/buttons.css']).pipe(
-        aws_s3.upload({ path:'components/buttons/' + pkg.version + '/'} ));
+    gulp.src(paths['css'] + '/buttons.css')
+        .pipe(aws_s3.upload({ path:'components/buttons/' + pkg.version + '/'} ));
 });
 
 gulp.task('run-release-bower', function(cb) {
     run('git tag -a v'+ pkg.version +' -m "release v' + pkg.version +' for bower"; git push origin master v'+ pkg.version).exec();
 });
 
-gulp.task('connect', function() {
-    connect.server({
-        root: '_site',
-        port: 3456,
-        livereload: true
-    });
-});
 
 gulp.task('serve', function(callback) {
     return runSequence(
         'build',
-        ['sass'],
         ['browserSync', 'watch'],
         callback
     );
