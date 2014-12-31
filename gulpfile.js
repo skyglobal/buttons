04e9860993213e2b028415568e1b20473e9dfb8e
sabeur <thabti.sabeur@gmail.com>
2014-12-31 11:32:42 +0000

    Version bump for release

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,4 @@
 var gulp = require('gulp');
 var componentHelper = require('gulp-component-helper')(gulp);
 var paths = componentHelper.paths;
-var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
